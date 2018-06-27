0456da8006aa11aab6c310b97ceca3706524a5f8
kristen154 <kristen154@users.noreply.github.com>
2018-06-27 11:20:52 +0800

    Update index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,10 @@
 
 const app = getApp()
-var template = require('../../template/template.js');
 Page({
   data: {
 
   },
   onLoad: function () {
-    template.tabbar("tabBar", 0, this)//0表示第一个tabbar
+    app.globalData.template.tabbar("tabBar", 0, this)//0表示第一个tabbar  
   },
 })
